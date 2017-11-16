c295e1c8cc98ea2697ffa44e99fbc22204a75ee0
AlligatorTaffy <AlligatorTaffy>
2017-11-15 21:32:22 -0600

    Source code for the Second InClass assignment.

---
+++ Week_16/InClass2.cpp
@@ -0,0 +1,41 @@
+/**
+ * Brice Lohman IN200
+ * Chapter 9 - Adv. Classes and the HEAP
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int* intOnHeap() {
+	int* pTemp = new int(20);
+	return pTemp;
+}
+
+void leak1() {
+	int* pTemp = new int(30);		// the new int never gets deleted
+}
+
+coid leak2() {
+	int* pTemp = new int(50);
+	pTemp = new int(100);			// assigns a new int and the old one still remains.
+	delete pTemp;
+}
+int main() {
+	cout << "Memory Games\n";
+
+	int* pAge = new int;		// Creating a pointer and a new memoery location
+	*pAge = 29;
+	cout << "pAge = " << *pAge << "     " << pAge << endl;
+	int* pSpeed = intOnHeap();
+	cout << "pSpeed = " << *pSpeed << endl;
+
+	cout << "Freeing memory pointed to by pAge\n";
+	delete pAge;
+	cout << "Freeing memory pointed to by pSpeed.\n";
+	delete pSpeed;
+
+	return 0;
+}
