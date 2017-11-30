9ccf537d17b065242b9a4e10ad1af1731e334ee7
AlligatorTaffy <AlligatorTaffy>
2017-11-30 06:56:22 -0600

    Uploading the source to the in-class final project

---
+++ Final/final.cpp
@@ -0,0 +1,111 @@
+/**
+ * Brice Lohman -- Coding Final for IN200
+ * Let's Make the Magic Happen, it has been a blast!
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+// Declarations
+void AddGame(vector<string>& givenVec, string givenGame);
+void DisplayFavs(vector<string> *givenVec);
+
+// Global Definitions
+
+//Number 1
+string Names[] = {"Frank", "Mike", "Brice", "Brian", "Sally", "Erin", "Sheila", "Fred", "Ashley", "Betty"};
+
+//Number7
+class Bot {
+private:
+	string m_Name;
+	int m_Health;
+	int m_Damage;
+public:
+	Bot(string name = "Starscream", int health = 100, int damage = 0) {
+		m_Name = name;
+		m_Health = health;
+		m_Damage = damage;
+	}
+	void Battlecry() {
+		cout << "I am " << m_Name << "! I have " << m_Health << " health and I have " << m_Damage << " damage!\n";
+	}
+	void Attack(Bot& givenBot, int givenDamage) {
+		givenBot.TakeDamage(givenDamage);
+	}
+	void TakeDamage(int givenDamage) {
+		m_Health -= givenDamage;
+	}
+};
+
+
+int main() {
+
+//Number 2
+	int numNames = 10;
+	for(int i = 0; i < numNames; i++) {
+		cout << Names[i] << endl;
+	}
+
+//Number 3
+	vector<string> favs;
+
+//Number 4B
+	AddGame(favs, "Super Mario RPG");
+	AddGame(favs, "Phantasy Star Online");
+	AddGame(favs, "Sonic the Hedgehog 2");
+	AddGame(favs, "Legend of Zelda: A Link to the Past");
+	AddGame(favs, "Legend of Zelda: Link's Awakening");
+
+//Number5B
+	DisplayFavs(&favs);
+
+//Number6
+	string input = "null";
+	vector<string>::iterator iter;
+	cout << "Search for a Game Title::";
+	getline(cin, input);
+	iter = find(favs.begin(), favs.end(), input);
+	if(*iter == input) {
+		cout << "Yes!  That's a favorite!\n";
+	} else {
+		cout << "Sorry!  Didn't see that game in the list!\n\nNow it is in the list!\n";
+		favs.push_back(input);
+	}
+	DisplayFavs(&favs);
+
+//Number7
+	Bot starscream;
+	Bot megatron("Megatron", 10000, 0);
+	starscream.Battlecry();
+	megatron.Battlecry();
+	starscream.Attack(megatron, 100);
+	megatron.Battlecry();
+	starscream.Battlecry();
+
+	return 0;
+}
+
+/**
+ * Functions
+ */
+
+
+//Number 4A
+void AddGame(vector<string>& givenVec, string givenGame) {
+	givenVec.push_back(givenGame);
+}
+
+//Number5A
+void DisplayFavs(vector<string> *givenVec) {
+	sort((*givenVec).begin(), (*givenVec).end());
+		for(int i = 0; i < (*givenVec).size(); i++) {
+			cout << (*givenVec)[i] << endl;
+	}
+}
---
+++ Final/final2.cpp
@@ -0,0 +1,52 @@
+/**
+ * Brice Lohman -- Coding Final for IN200
+ * So this is supposed to be easy?
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+//Number 1
+	int input;
+	cout << "Select a Door 1, 2, or 3::";
+	cin >> input;
+	switch(input) {
+		case 0: cout << "Shh!  This is the SECRET door ;)\n";
+			break;
+		case 1: cout << "You've picked DOOR #1 -- FANTASTIC!\n";
+			break;
+		case 2: cout << "You've picked DOOR #2 -- CONGRATS!\n";
+			break;
+		case 3: cout << "You've picked DOOR #3 -- REGRETS!\n";
+			break;
+		default: cout << "That's not a door!\n";
+	}
+
+//Number2
+	string input2;
+	cout << "Out of these, which is your favorite? (DOG, CAT, BUNNY)::";
+	cin.ignore();
+	getline(cin, input2);
+	if(input2 == "DOG") {
+		cout << "Look's like we have ourselves a dog person.\n";
+	} else if(input2 == "CAT") {
+		cout << "Are you Bubbles?  How's Sunnyvale?\n";
+	} else if(input2 == "BUNNY") {
+		cout << "Hip.  Hip Hop.\n";
+	} else {
+		cout << "You chose wrongly.\n";
+	}
+
+//Number3
+	srand(time(0));
+	cout << "\nEnjoy this random number::" << (rand() % 100) << endl;
+
+	return 0;
+}
